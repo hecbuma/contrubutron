ac9ff6c7a0492682e94f3c04ebeee3639f83e946
Hector Bustillos <hecbuma@gmail.com>
2015-11-08 17:33:57 -0600

    one last thing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,7 +9,12 @@ class DashboardController < ApplicationController
       if current_user.organizations.where("aasm_state like 'fetching'").size > 0 && !@organization.fetching?
         flash[:alert] = "You can only process one organization at the time."
       elsif @organization.created?
-        @organization.move_to_queue(session[:user_token], current_user)
+        if @organization.name != 'railsrumble'
+          @organization.move_to_queue(session[:user_token], current_user)
+          flash[:alert] = nil
+        else
+          flash[:alert] = "I'll love to process this organization but I'm worry about the size :p"
+        end
       end
     end
 
