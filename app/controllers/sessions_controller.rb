7713a5732ac1783d86f9fdcf963b57146800a439
Hector Bustillos <hecbuma@gmail.com>
2015-11-08 16:00:47 -0600

    edit profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
-  before_filter :avoid_logged_in_user_to_sign_in, :except => :destroy
-  skip_before_filter :get_organizations
+  before_filter :avoid_logged_in_user_to_sign_in, :except => [:destroy, :edit, :update]
+  skip_before_filter :get_organizations, :only => [:create, :destroy]
 
 
   def create
@@ -17,11 +17,33 @@ class SessionsController < ApplicationController
     redirect_to root_path, notice: 'Signed out!'
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user == current_user
+      if @user.update_attributes user_params
+        message = {notice: 'Everything is cool, info updated.'}
+      else
+        message = {alert: 'looks like something went wrong.'}
+      end
+      redirect_to :back, message
+    else
+      redirect_to root_path, alert: "you don't have permission to edit that data"
+    end
+  end
+
   private
   def avoid_logged_in_user_to_sign_in
     if current_user
       redirect_to dashboard_index_path, alert: "You have already logged in"
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
 
   get 'auth/:provider/callback' => 'sessions#create'
-  get '/signout' => 'sessions#destroy', :as => :signout
+  get '/signout'     => 'sessions#destroy', :as => :signout
+  get   'profile'  => 'sessions#edit',  :as => :edit_profile
+  patch 'update/:id'  => 'sessions#update',   :as =>  :update_profile
 
   resources :dashboard
   resources :members
