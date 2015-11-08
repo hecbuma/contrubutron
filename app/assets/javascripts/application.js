71f2994ebd42c2cc86ee32f40de5f4c914ab1f10
Hector Bustillos <hecbuma@gmail.com>
2015-11-08 14:40:38 -0600

    add about page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require turbolinks
+
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require adminLTE/manifest
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
 
+
   get 'auth/:provider/callback' => 'sessions#create'
   get '/signout' => 'sessions#destroy', :as => :signout
 
   resources :dashboard
   resources :members
+  resources :about
 
 
   root 'site#index'
